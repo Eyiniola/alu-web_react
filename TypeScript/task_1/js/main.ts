b235b59c24b9cbb42c610d81699f99b103202030
Eyiniola Fagbemi <127391120+Eyiniola@users.noreply.github.com>
2025-05-10 16:07:12 +0000

    Creating interface for teachers

---
+++ TypeScript/task_1/js/main.ts
@@ -0,0 +1,19 @@
+interface Teacher {
+    readonly firstName: string;
+    readonly lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string;
+    [key: string]: any;
+}
+
+const teacher: Teacher = {
+  firstName: 'John',
+  lastName: 'Severance',
+  fullTimeEmployee: true,
+  yearsOfExperience: 5,
+  location: 'New York',
+  contract: true, // add a new attribute
+  salary: 50000, // add another new attribute
+};
+
