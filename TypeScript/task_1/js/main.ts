ebd4e1c967af60681b20be60877b581dfdf09bd9
Eyiniola Fagbemi <127391120+Eyiniola@users.noreply.github.com>
2025-05-11 15:33:39 +0000

    printing teachers

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -21,3 +21,12 @@ interface Directors extends Teacher {
   numberOfReports: number;
 }
 
+interface printTeacherFunction {
+  (firstName: string,
+  lastName: string,): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName.charAt(0)}. ${lastName}`;
+};
+
