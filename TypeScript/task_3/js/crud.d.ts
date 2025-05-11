4cfcd145ffb6d539b55e6a1f0efa74f273e2f91f
Eyiniola Fagbemi <127391120+Eyiniola@users.noreply.github.com>
2025-05-11 20:13:37 +0000

    task

---
+++ TypeScript/task_3/js/crud.d.ts
@@ -0,0 +1,5 @@
+import { RowElement, RowId } from "./interface";
+
+declare function insertRow(row: RowElement): number;
+declare function updateRow(rowId: RowId, row: RowElement): number;
+declare function deleteRow(rowId: RowId): void;
